75c443a0a266c0afe986bfcb96d23c8e738eff7b
Yeheskiel Yunus Tame <117884201+yeheskieltame@users.noreply.github.com>
2025-03-19 14:30:04 +0700

    rev

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getGreeting() {
   const hour = new Date().getHours();
-  if (hour < 12) return " Good morning";
-  if (hour < 18) return " Good afternoon";
-  return " Good evening";
+  if (hour < 12) return "Good morning";
+  if (hour < 18) return "Good afternoon";
+  return "Good evening";
 }
