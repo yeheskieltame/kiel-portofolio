1e2d0b9feba04a3c2a1ac02f7a101e45899fd0ac
yeheskieltame <yeheskiel.yunus@ti.ukdw.ac.id>
2025-04-18 19:46:34 +0700

    change host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/p-597196/' : '/',
   server: {
     host: "::",
     port: 8080,
